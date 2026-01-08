2b78b48aaaaa541d2b6c72870c336e04a337fa6c
CaikeHenry007 <caikehenry12@gmail.com>
2026-01-07 21:29:43 -0300

    Atualização - Backend Uploads de imagens

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,23 @@
 import dotenv from "dotenv";
-dotenv.config();
 
 import express from "express";
 import cors from "cors";
+import path from "path";
 import produtoRoutes from "./routes/produtoRoutes.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import adminRoutes from "./routes/adminRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
+
+dotenv.config();
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use("/uploads", express.static("uploads"));
+app.use("/uploads", express.static(path.resolve("uploads")));
 
 app.use("/api/produtos", produtoRoutes);
+app.use("/api/upload", uploadRoutes);
 app.use("/categories", categoryRoutes);
 app.use("/admin", adminRoutes);
 
@@ -22,6 +26,11 @@ app.get("/", (req, res) => {
 });
 
 const PORT = 3000;
+
+app.get("/teste", (req, res) => {
+  res.json({ ok: true });
+});
+
 app.listen(PORT, () => {
   console.log("Servidor rodando na porta " + PORT);
 });
---
+++ backend/uploads/multer.js
@@ -0,0 +1,33 @@
+import multer from "multer";
+import path from "path";
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    const ext = path.extname(file.originalname);
+    const filename = `${Date.now()}-${Math.random()
+      .toString(36)
+      .substring(2)}${ext}`;
+
+    cb(null, filename);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/jpeg" ||
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/webp"
+  ) {
+    cb(null, true);
+  } else {
+    cb(new Error("Formato de imagem inválido"), false);
+  }
+};
+
+export default multer({
+  storage,
+  fileFilter,
+});
